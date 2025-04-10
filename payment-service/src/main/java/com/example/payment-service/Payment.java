f0e5bb94f27954c23c34a1e1c483da34d88c41a1
Master <kumarparakram007@gmail.com>
2025-04-10 14:04:06 +0530

    initial on main

---
+++ payment-service/src/main/java/com/example/payment-service/Payment.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Payment {
+    public static void main(String[] args) {
+        System.out.println("Hello from payment-service!");
+    }
+}
---
+++ user-service/src/main/java/com/example/user-service/User.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class User{
+    public static void main(String[] args) {
+        System.out.println("Hello from user-service!");
+    }
+}
